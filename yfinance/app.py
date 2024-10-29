ed98622978ad5444eb26d5a4a9725bb177f16998
Vinicius Amorim <viniciusdamorim@gmail.com>
2024-10-29 20:29:38 -0300

    novas alterações

--- yfinance/app.py
+++ yfinance/app.py
@@ -1,15 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 import tensorflow as tf
+import os
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 app = Flask(__name__)
 
 # Carrega o modelo
-model = tf.keras.models.load_model('modelo/lstm.h5')
+model_path = os.path.join('C:/GIT/Machine-Learning-Engineering/yfinance/modelo', 'lstm.h5')
+model = tf.keras.models.load_model(model_path)
 
 # Função para fazer previsões
 def make_prediction():
-    # Chama o método de previsão do seu modelo
-    predictions = model.predict_future()  # Altere isso para o método correto do seu modelo
+    # Aqui você chama o método que já existe no seu modelo para gerar as previsões
+    predictions = model.predict_future()  # Substitua pelo método correto que gera as previsões
     return {
         "predictions_5_days": predictions[0],
         "predictions_10_days": predictions[1],
@@ -17,14 +21,22 @@ def make_prediction():
         "predictions_20_days": predictions[3],
     }
 
-@app.route('/predict', methods=['GET'])
+    response = make_response(jsonify(predictions))
+    response.headers['Content-Type'] = 'application/json'
+
+@app.route('/predict', methods=['POST'])  # Mantenha o método GET
 def predict():
     try:
         # Chama a função de previsão
         predictions = make_prediction()
         
-        # Retorna a previsão em formato JSON
-        return jsonify(predictions)
+        # Return json
+        response = make_response(jsonify(predictions))
+
+        # Define o cabeçalho Content-Type
+        response.headers['Content-Type'] = 'application/json'
+        
+        return response
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
